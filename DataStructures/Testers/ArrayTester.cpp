01c97fff04cb5227f3fd7adfa265b8778d56d70d
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-02-06 08:24:42 -0700

    Made a testers foldier and made arrayTesters.

---
+++ DataStructures/Testers/ArrayTester.cpp
@@ -0,0 +1,9 @@
+//
+//  ArrayTester.cpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "ArrayTester.hpp"
---
+++ DataStructures/Testers/ArrayTester.hpp
@@ -0,0 +1,14 @@
+//
+//  ArrayTester.hpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef ArrayTester_hpp
+#define ArrayTester_hpp
+
+#include <stdio.h>
+
+#endif /* ArrayTester_hpp */
