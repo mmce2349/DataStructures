cbecefd7a266d9a5496637fe803f0eb95d40f080
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-02-14 08:46:33 -0700

    added more code.

---
+++ DataStructures/Model/Structures/Linear/List.cpp
@@ -0,0 +1,9 @@
+//
+//  List.cpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 2/14/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "List.hpp"
---
+++ DataStructures/Model/Structures/Linear/List.hpp
@@ -0,0 +1,156 @@
+//
+//  List.hpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 2/14/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+using namespace std;
+
+template<class Type>
+class LinkedList: public List<Type>
+{
+protected:
+    LinearNode<Type> * front;
+    LinearNode<Type> * end;
+public:
+    LinkedList();
+    virtual ~LinkedList();
+    int getSize() const;
+    LinearNode<Type> * getFront();
+    LinearNode<Type> * getEnd();
+    
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+    Type remove(int index);
+    
+};
+
+LinkedList<Type> :: LinkedList()
+{
+    this->front = nullptr;
+    this->end = nullptr;
+    this->size = 0;
+}
+
+LinkedList<Type> :: ~LinkedList()
+{
+    LinearNode<Type> * destroyStructure = front;
+    while(front != nullptr)
+    {
+        front = destroyStructure->getNextNode();
+        delete destroyStructure;
+        destroyStructure = front;
+    }
+}
+void LinkedList<Type> :: add(Type item)
+{
+    LinearNode<Type> * newData = new LinearNode<Type>(item);
+    
+    if(this->size == 0)
+    {
+        this->front = newData;
+    }
+    else
+    {
+        this->end->setNextNode(newData);
+    }
+    
+    this->end = newData:
+    
+    this->size += 1;
+    
+}
+void LinkedList<Type> :: addAtIndex(int index, Type item)
+{
+    assert(index >= 0 && index <= this-> size);
+    if(index == this-> size)
+    {
+        add(item);
+    }
+    else
+    {
+        LinearNode<Type> * toBeAdded = new LinearNode<Type>(item);
+        if (index == 0)
+        {
+            toBeAdded->setNextNode(front);
+            front = toBeAdded;
+        }
+        else
+        {
+            LinearNode<Type> * previous = nullptr;
+            LinearNode<Type> * current = front;
+            for(int position = 0; position < index; position++ )
+            {
+                previous =current;
+                current = current->getNextNode();
+            }
+            previous-> setNextNode(toBeAdded);
+            toBeAdded->setNextNode(current);
+        }
+        this->size++;
+    }
+    
+    
+}
+Type LinkedList<Type> :: remove(int index)
+{
+    assert(index >= 0 && index < this->size);
+    
+    LinearNode<Type> * current = front;
+    LinearNode<Type> * toBeRemoved = nullptr;
+    LinearNode<Type> * previous = nullptr;
+    
+    Type removedData;
+    
+    if(index ==0)
+    {
+        toBeRemoved = front;
+        this->front = this->front->getNextNode();
+    }
+    else
+    {
+        for(int position =0; position < index; position++)
+        {
+            previous= current;
+            current = current->getNextNode;
+        }
+        
+        toBeRemoved = current;
+        
+        if(index == this->size-1)
+        {
+            previous->setNextNode(nullptr);
+            end = previous;
+        }
+        else
+        {
+            current = toBeRemoved->getNextNode();
+            previous->setNextNode(current);
+        }
+    }
+    this->size -=1;
+    
+    removeData = toBeRemoved-> getData();
+    delete toBeRemoved;
+    return removedData;
+}
+LinearNode<Type> * LinkedList<Type> :: getEnd()
+{
+    return this->end;
+}
+LinearNode<Type> * LinkedList<Type> :: getFront()
+{
+    return this->Front;
+}
+int LinkedList<Type> :: getSize() const
+{
+    return this->size;
+}
+#include <stdio.h>
+
+#endif /* List_hpp */
