400e552f14c4991ebacbada5bd132c1121d94431
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-03-05 07:55:18 -0700

    added more code and files.

---
+++ DataStructures/Testers/RecursionTester.hpp
@@ -0,0 +1,26 @@
+//
+//  RecursionTester.hpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 3/5/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef RecursionTester_hpp
+#define RecursionTester_hpp
+
+#include <iostream>
+#include "../Controller/Tools/Timer.hpp"
+#include <string>
+using namespace std;
+class RecursionTester
+{
+private:
+    int fib(int start);
+    string mystery(string word);
+    
+public:
+    void testRecursionNumbers();
+    void testRecursionString();
+};
+#endif /* RecursionTester_hpp */
