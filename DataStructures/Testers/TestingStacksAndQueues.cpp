ca7a981348c3b8d61e57c6e0b0d81403f08c8656
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-03-15 09:10:33 -0600

    Did hpp

---
+++ DataStructures/Testers/TestingStacksAndQueues.cpp
@@ -0,0 +1,9 @@
+//
+//  TestingStacksAndQueues.cpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 3/15/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "TestingStacksAndQueues.hpp"
---
+++ DataStructures/Testers/TestingStacksAndQueues.hpp
@@ -0,0 +1,29 @@
+//
+//  TestingStacksAndQueues.hpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 3/15/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef TestingStacksAndQueues_hpp
+#define TestingStacksAndQueues_hpp
+
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+#include "../Model/Structures/Linear/Queue.hpp"
+#include "../Model/Structures/Linear/Stack.hpp"
+#include "../Model/Structures/Linear/ListAbstractBase.hpp"
+#include <iostream>
+using namespace std;
+
+class TestingStackAndQueues
+{
+public:
+    void stackVersusList();
+    void stackVersusQueue();
+    void queueVersusList();
+};
+
+
+#endif /* TestingStacksAndQueues_hpp */
