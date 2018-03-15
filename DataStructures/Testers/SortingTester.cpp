988a421008d61dbb0a19a27a78396c1844e0934b
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-03-15 08:28:19 -0600

    fixed my errors

--- DataStructures/Testers/SortingTester.cpp
+++ DataStructures/Testers/SortingTester.cpp
@@ -39,16 +39,19 @@ void SortingTester :: swap(vector<CrimeData> info, int small, int large)
     CrimeData temp = info[small];
     info[small] = info[large];
     info[large] = temp;
+    swapCount++;
 }
 
 void SortingTester :: testSorts()
 {
     Timer sortTimer;
+    swapCount =0;
     vector<CrimeData> data = FileController :: readCrimeDataToVector("/Users/mmce2349/Documents/Data Structures 2018/DataStructures/DataStructures/Data/crime.csv");
     
     sortTimer.startTimer();
     quickSort(data, 0, data.size());
     sortTimer.stopTimer();
     sortTimer.displayInformation();
+    cout<< "The number of swaps were: " << swapCount << endl;
 }
 
