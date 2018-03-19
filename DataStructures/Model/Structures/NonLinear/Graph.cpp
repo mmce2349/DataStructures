1cf0233cc23e7e475a9952015f6001b915503c41
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-03-19 08:43:01 -0600

    addded a  lot of stuff

---
+++ DataStructures/Model/Structures/NonLinear/Graph.cpp
@@ -0,0 +1,9 @@
+//
+//  Graph.cpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 3/19/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Graph.hpp"
---
+++ DataStructures/Model/Structures/NonLinear/Graph.hpp
@@ -0,0 +1,52 @@
+//
+//  Graph.hpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 3/19/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <set>
+#include <queue>
+#include <assert.h>
+using namespace std;
+template<class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 20;
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVertices[]);
+public:
+    Graph();
+    void addVertex(const Type& value);
+    
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target);
+    
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    Type& operator [] (int vertex);
+    Type operator [] (int vertex) const;
+    int size() const;
+    
+    bool hasUndirectedConnection(int source, int target)const;
+    bool areConnected(int source, int target)const;
+    std::set<int> neightbors(int vertex) const;
+    
+    void depthFirstTraversal(Graph<Type> & graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> & graph, int vertex);
+    int costTraversal(Graph<Type> & graph, int vertex);
+    
+};
+
+#endif /* Graph_hpp */
