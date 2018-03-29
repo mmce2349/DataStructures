a04274c91f8ec699ede0e2f573732a7ea27a0c16
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-03-29 09:02:51 -0600

    fixed some stuff

---
+++ DataStructures/Testers/BinaryTreeTester.cpp
@@ -0,0 +1,10 @@
+//
+//  BinaryTreeTester.cpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 3/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "BinaryTreeTester.hpp"
+
---
+++ DataStructures/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,24 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 3/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include <iostream>
+#include "../Model/Structures/NonLinear/BinaryTree.hpp"
+#inculde "../Model/Structures/NonLinear/BinarySearchTree.hpp"
+
+class BinaryTreeTester
+{
+private:
+    BinarySearchTree<int> testTree;
+public:
+    void doTreeStuff();
+}
+
+#endif /* BinaryTreeTester_hpp */
