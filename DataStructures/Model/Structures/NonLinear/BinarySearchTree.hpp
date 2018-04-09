2148c2997e8244fef4ba01b7a8cdde7bf30a3f36
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-04-09 08:57:00 -0600

    added more code

--- DataStructures/Model/Structures/NonLinear/BinarySearchTree.hpp
+++ DataStructures/Model/Structures/NonLinear/BinarySearchTree.hpp
@@ -145,21 +145,84 @@ void BinarySearchTree<Type> :: remove(Type item)
 template <class type>
 int BinarySearchTree<Type> :: getHeight()
 {
-    return -1;
+    reutrn calculateHeight(this->root);
+    
+}
+template <class type>
+int BinarySearchTree<Type> :: calculateHeight(BinaryTreeNode<Type> * current)
+{
+    if(current != nullptr)
+    {
+        return max(calculateHeight(current->getLeftNode()), calculateHeight(current->getRightNode())) + 1;
+    }
+    return 0;
 }
 template <class type>
 int BinarySearchTree<Type> :: getSize()
 {
-    return -1;
+    int size = 0;
+    size+= calculateSize(this->root);
+    return size;
+}
+template <class type>
+int BinarySearchTree<Type> :: calculateSize(BinaryTreeNode<Type> * current)
+{
+    if(current != nullptr)
+    {
+        return calculateSize(current->getLeftNode())+ calculateSize(current->getRightNode()) + 1;
+        
+    }
+    return 0;
 }
 template <class type>
 bool BinarySearchTree<Type> :: isComplete()
 {
-    return false;
+    int index = 0;
+    int size= getSize();
+    
+    return iscomplete(this->root, index, size);
+}
+template <class type>
+bool BinarySearchTree<Type> :: isComplete(BinaryTreeNode<Type> * startNode, int index, int size)
+{
+    if(startNode == nullptr)
+    {
+        return true;
+    }
+    if(index>=size)
+    {
+        return false;
+    }
+    return (isComplete(startNode-> getLeftNode(), 2 * index + 1, size) && isComplete(startNOde->getRightNode(), 2 * index + 2, size));
 }
 template <class type>
 bool BinarySearchTree<Type> :: isBalanced()
 {
+    return isBalanced(this->root);
+    
+}
+template <class Type>
+bool BinarySearchTree<Type> :: isBalanced(BinaryTreeNode<Type> * current)
+{
+    int leftHeight = 0;
+    int rightHeight = 0;
+    
+    if(current == nullptr)
+    {
+        return true;
+    }
+    
+    leftHeight = calculateHeight(current->getLeftNode());
+    rightHeight = calculateHeight(current->getRightNode());
+    
+    int heightDifference = abs(leftHeight-rightHeight);
+    bool leftBalanced = isBalanced(current->getLeftNode());
+    bool rightBalanced = isBalanced(current->getRightNode());
+    
+    if (heightDifference <= 1 && leftBalanced && rightBalanced)
+    {
+        return true;
+    }
     return false;
 }
 
