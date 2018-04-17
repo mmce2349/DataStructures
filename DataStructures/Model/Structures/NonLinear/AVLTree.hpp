3c40ae8abb77b3948d2443ca3d26ec9711ee30f1
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-04-17 09:29:34 -0600

    finally done

--- DataStructures/Model/Structures/NonLinear/AVLTree.hpp
+++ DataStructures/Model/Structures/NonLinear/AVLTree.hpp
@@ -179,6 +179,10 @@ void AVLTree <Type> :: insert(Type item)
     insertNode(this->getRoot(), item);
     
 }
-
+template <class Type>
+void AVLTree<Type> :: remove(Type item)
+{
+    removeNode(this->getRoot(), item);
+}
 
 #endif /* AVLTree_hpp */
