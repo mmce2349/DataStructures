e96a5007f699519f6012490de804ed278a5ba9d4
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-04-23 08:18:36 -0600

    added N_Ary

---
+++ DataStructures/Model/Nodes/N_AryTreeNode.hpp
@@ -0,0 +1,37 @@
+//
+//  N_AryTreeNode.hpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 4/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef N_AryTreeNode_hpp
+#define N_AryTreeNode_hpp
+
+#include <set>
+#include "Node.hpp"
+using namespace std;
+
+template<class Type>
+class N_AryTreeNode : public Node<type>
+{
+private:
+    set<N_AryTreeNode<Type>*> nodes;
+    N_AryTreeNode<Type> * root;
+public:
+    N_AryTreeNode<Type>();
+    ~N_AryTreeNode<Type>();
+    N_AryTreeNode<Type>(Type data);
+    
+    set<N_AryTreeNode<Type>*> getNodes();
+    N_AryTreeNode<Type>* getRoot();
+    int getChildCount();
+    
+    void setRoot(N_AryTreeNode<Type>* root);
+    void addChild(N_AryTreeNode<Type> * node);
+};
+
+
+
+#endif /* N_AryTreeNode_hpp */
