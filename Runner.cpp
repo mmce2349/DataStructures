5a172f02c1b261819e035721185dce82ea9f1b70
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-04-27 08:41:52 -0600

    fixed code

--- Runner.cpp
+++ Runner.cpp
@@ -10,24 +10,8 @@
 using namespace std;
 int main()
 {
-    int sample = 7;
-    string word = "asda";
-    string word2 = "asda";
-    cout << &word << endl;
-    cout << &word2 << endl;
-    
-    unsigned long * storage;
-    storage = new unsigned long [0xffffffff];
-    storage [address] = 0;
-    
-    cout << &sample << endl;
-    unsigned long address = (unsigned long) &sample;
-    cout << address << endl;
-    if(&sample == address)
-    {
-        cout << "the same" << endl;
-    }
-   // Controller app;
-  //  app.start();
+   
+    Controller app;
+    app.start();
     return 0;
 }
