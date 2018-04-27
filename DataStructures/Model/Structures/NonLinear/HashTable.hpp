fbaa2aa03d5311bb142d5f7ad95d4b1c4c8f2f1e
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-04-27 09:20:12 -0600

    add more code

---
+++ DataStructures/Model/Structures/NonLinear/HashTable.hpp
@@ -0,0 +1,36 @@
+//
+//  HashTable.hpp
+//  DataStructures
+//
+//  Created by Mcentire, Mitchell on 4/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef HashTable_hpp
+#define HashTable_hpp
+
+#include "../Nodes/HashNode.hpp"
+template <class type>
+class Hashtable
+{
+private:
+public:
+    Hashtable();
+    ~Hashtable();
+
+};
+
+template<class Type>
+Hashtable<Type> :: Hashtable()
+{
+    internalStorage = new HashNode<Type>[101];
+    size = 0;
+}
+
+template <class Type>
+Hashtable<Type> :: ~Hashtable()
+{
+    delete [] internalStorage;
+}
+
+#endif /* HashTable_hpp */
